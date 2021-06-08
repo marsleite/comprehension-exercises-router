3d94419885fdf3c2f4e4dfc5e48f75cc59c8017c
marsleite <marsleite@gmail.com>
2021-06-08 07:19:39 -0300

    exercicio fixacao links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/users">Users</Link></li>
+            </ul>
+          </nav>
+        <Route exact path="/" component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/users" component={ Users } />
+      </BrowserRouter>
     );
   }
 }
